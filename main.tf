7b1c61908bf4e5aa637979a642b1e6cfb7bcd5ac
Ha Tien Thang <>
2025-10-28 16:43:36 +0700

    Refactor web security group configuration in main.tf by replacing the aws_security_group resource with a security group module, updating ingress and egress rules for improved management and clarity.

--- main.tf
+++ main.tf
@@ -24,39 +24,22 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami[0].id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.web[0].id]
+  vpc_security_group_ids = [module.security_group.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-resource "aws_security_group" "web" {
+module "security_group" {
   count = var.enable_web ? 1 : 0
-  name = "web"
-  description = "Allow HTTP and HTTPs in. Allow everything out."
+  source = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+  name = "web_new"
 
   vpc_id = data.aws_vpc.default.id
-}
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  egress_rules = ["all-all"]
+  ingress_with_cidr_blocks = ["0.0.0.0/0"]
 
-resource "aws_security_group_rule" "web_http_ingress" {
-  count = var.enable_web ? 1 : 0
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web[0].id
-}
-
-resource "aws_security_group_rule" "web_egress" {
-  count = var.enable_web ? 1 : 0
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web[0].id
 }
