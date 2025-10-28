b1abddd50749fffdf2514fbb1de61abc89f43a71
Ha Tien Thang <>
2025-10-28 10:13:27 +0700

    Remove AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID variables from variables.tf to enhance security and simplify configuration.

--- variables.tf
+++ variables.tf
@@ -7,15 +7,3 @@ variable "bitnami_ami_owner" {
   description = "Owner of the Bitnami AMI"
   default     = "979382823631"
 }
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Access Key"
-  type = string
-  sensitive = true
-}
-
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key ID"
-  type = string
-  sensitive = true
-}
